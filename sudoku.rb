2209c66a13d5e8140e61669eb80c844156acb0ef
codeamac01 <codeamac01@gmail.com>
2017-03-21 11:39:16 -0600

    Empezando con sudoku.

---
+++ sudoku.rb
@@ -0,0 +1,11 @@
+class Sudoku
+  def initialize(board)
+    @board = board
+  end
+
+
+
+end
+
+# Driver code
+game = Sudoku.new('702806519100740230005001070008000002610204053200000100070400800064078005821503907')
